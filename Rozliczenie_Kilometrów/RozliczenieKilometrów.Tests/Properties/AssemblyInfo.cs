bbe8c7edb0f7d921ebeb65f807d1b45dbdf8e77f
DonOreo <44070308+DonOreo@users.noreply.github.com>
2019-05-28 09:36:30 +0200

    końcóweczka

---
+++ Rozliczenie_Kilometrów/RozliczenieKilometrów.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RozliczenieKilometrów.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RozliczenieKilometrów.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5ed92f2a-9ab0-4fd5-b245-2a1142498472")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Rozliczenie_Kilometrów/RozliczenieKilometrów.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RozliczenieKilometrów;
+using Settlement_of_kilometers;
+
+namespace RozliczenieKilometrów.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddNewRideTests()
+        {
+            // Do wymyślenia testy wczytywane z pliku
+            var ride = new Ride();
+            fill(ride, 10, 20, 1, DateTime.Now);
+
+            Assert.IsTrue(ride.DataCompleted());
+        }
+
+        public void fill(Ride ride, double start, double end, double capacity, DateTime date)
+        {
+            ride.StardRoad = start;
+            ride.EndRoad = end;
+            ride.Capacity = capacity;
+            ride.Date = date;
+        }
+    }
+}
